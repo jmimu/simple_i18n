76af4d818077af34a14830c30134e0a6a78c7e0d
jmmuller <jean-michael.muller@ign.fr>
2024-04-15 17:19:58 +0200

    py3 + details

--- prep_translation.py
+++ prep_translation.py
@@ -3,7 +3,7 @@ import re
 import sys
 
 if len(sys.argv)<3 :
-  print "Usage :\n./prep_translation.py file.js la\nWhere 'la' is the language."
+  print("Usage :\n./prep_translation.py file.js la\nWhere 'la' is the language.")
   exit(0)
 
 filename=sys.argv[1]
@@ -17,22 +17,22 @@ inside_translation=False
 #get all existing keys
 for line in datafile:
   if re.compile('translation_'+language+'={').search(line):
-    #print "hop!"
+    #print("hop!")
     inside_translation=True
     continue
   if inside_translation and re.compile('}').search(line):
-    #print "bof."
+    #print("bof.")
     inside_translation=False
     break
   if inside_translation :
     if (re.match(' *"(.*)": *"(.*)"', line)) :
       key=re.match(' *"(.*)": *"(.*)"', line).group(1)
       val=re.match(' *"(.*)": *"(.*)"', line).group(2)
-      #print '  key found: "'+key+'": "'+val+'",'
+      #print('  key found: "'+key+'": "'+val+'",')
       existing_keys[key]=val
 
-#print "existing_keys: "
-#print existing_keys
+#print("existing_keys: ")
+#print(existing_keys)
 #get new keys
 datafile.seek(0)
 all_keys=[]
@@ -43,17 +43,17 @@ for line in datafile:
     all_keys+=re.findall('_\( *[\']([^\']+)[\'] *\)', line)
 all_keys_unique=[]
 [all_keys_unique.append(x) for x in all_keys if x not in all_keys_unique]
-
-#print len(all_keys),len(all_keys_unique)
-#print all_keys
+all_keys_unique = sorted(all_keys_unique)
+#print(len(all_keys),len(all_keys_unique))
+#print(all_keys)
 
 #write transltation mixing existing and new keys
-print "var translation_"+language+"={"
+print("const translation_"+language+"={")
 for key in all_keys_unique :
   if (key in existing_keys) :
-    print '  "'+key+'": "'+existing_keys[key]+'",'
+    print('    "'+key+'": "'+existing_keys[key]+'",')
   else :
-    print '  "'+key+'": "~'+key+'~",'
-print "}"
+    print('    "'+key+'": "~'+key+'~",')
+print("}")
 
 datafile.close()
